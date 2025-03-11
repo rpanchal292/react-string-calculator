0c3ff5a4fc409ac5323124d4682403a4cb11b881
rpanchal292 <rpanchal292@gmail.com>
2025-03-11 12:13:21 +0530

    set proper css and resolved issue regarding react

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client"; // Use "react-dom/client" in React 18
 import "./index.css";
 import StringCalculatorApp from "./App";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <StringCalculatorApp />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
